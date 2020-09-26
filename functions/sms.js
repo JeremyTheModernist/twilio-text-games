17ebf51ca4d935e702485e663439a0ab9df0274f
Jeremy Barnes <jeremy@wovenmagazine.com>
2020-09-26 16:18:46 -0700

    wip: twilio sms with body.

--- functions/sms.js
+++ functions/sms.js
@@ -1,12 +1,14 @@
 exports.handler = async () => {
-  if (count === 0) {
-    var count = 0;
-  } else {
-    count += 1;
-  }
   console.log('CHECK OUT ALL MY COUNTS', count);
   return {
     statusCode: 200,
-    body: 'OK Rocking and a rolling',
+    headers: {
+      'Content-Type': 'text/xml',
+    },
+    body: `
+        <Response>
+            <Message>I have recieved a text</Message>
+        </Response>
+    `,
   };
 };
