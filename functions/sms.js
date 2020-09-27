812c93648c34008581a87a4940825d5017a77b97
Jeremy Barnes <jeremy@wovenmagazine.com>
2020-09-27 11:49:02 -0700

    feat: actually count votes.

--- functions/sms.js
+++ functions/sms.js
@@ -15,14 +15,35 @@ exports.handler = async (event) => {
 
   //   twilio doesn't enforce a strict data structure, but when you update it it should maintain the same structure
   //   so you need to remember the document name you pass
-  const result = await twilio.sync
-    .services(process.env.TWILIO_SERVICE_SID)
-    .documents.create({
-      uniqueName: 'JSB Text To Vote Results',
-    });
+
+  //   const result = await twilio.sync
+  //     .services(process.env.TWILIO_SERVICE_SID)
+  //     .documents.create({
+  //       uniqueName: 'JSB Text To Vote Results',
+  //     });
+
+  // after the document is created then just use the update method
+
   console.log('TWILIO RESULT', result);
   const { Body } = qs.parse(event.body);
   console.log('CHECK OUT ALL MY MESSAGE', Body);
+  const vote = Body.match(/yes/i) ? 'yes' : 'no';
+
+  const { data } = await twilio.sync
+    .services(process.env.TWILIO_SERVICE_SID)
+    .documents('IS7473257f7b67069feeb8afe54f1732aa')
+    .fetch();
+
+  const newData = {
+    ...data,
+    [vote]: data[vote] + 1,
+  };
+
+  await twilio.sync
+    .services(process.env.TWILIO_SERVICE_SID)
+    .documents('IS7473257f7b67069feeb8afe54f1732aa')
+    .update(newData);
+
   return {
     statusCode: 200,
     headers: {
@@ -30,7 +51,7 @@ exports.handler = async (event) => {
     },
     body: `
         <Response>
-            <Message>Thank you for voting. Your replied with:  ${Body}</Message>
+            <Message>Thank you for voting. Your replied with:  ${vote} on sandwiches</Message>
         </Response>
     `,
   };
